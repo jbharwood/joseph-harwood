dab607bd2a4263d12c17c9f99ae3f00a8c1324eb
jbharwood <josephblakeharwood@gmail.com>
2019-05-17 17:22:15 -0400

    added navbar navigation and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import NavBar from './components/NavBar';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <nav>
+        <ul>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/about' component={About}></Route>
+        <Route exact path='/contact' component={Contact}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p> Full-stack web developer with a passion for creating unique user
+        experiences using Ruby on Rails, JavaScript, and React and a background
+        in database administration. A gaming enthusiast, I bring strong skills
+        in cross-functional projects that meet customer demands and deadlines in
+        small environments as well as giants such as Blue Cross Blue Shield of
+        South Carolina serving millions of people.</p>
+      </div>
+    )
+  }
+}
+export default About
