9b18bb9ac17844e9ee6fa7f5c9a24c00ab5839df
jbharwood <josephblakeharwood@gmail.com>
2019-05-17 17:29:36 -0400

    added links to media

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,12 @@ class Contact extends React.Component {
   render() {
     return (
       <div>
-
+        <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
+        <a href="https://github.com/jbharwood" target="_blank"> Github </a>
+        <a href="https://medium.com/@josephharwood_62087" target="_blank"> Medium </a>
       </div>
     )
   }
+
 }
 export default Contact
