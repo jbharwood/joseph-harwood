dfc4d4be3eb42f9e3a182adef7174d6eb2dac0ae
jbharwood <josephblakeharwood@gmail.com>
2019-05-20 17:04:00 -0400

    added picture of me

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import me from '../media/LinkedIn.jpg'; // with import
 
 class Home extends React.Component {
 
@@ -6,6 +7,7 @@ class Home extends React.Component {
     return (
       <div>
         <h1> Joseph Harwood </h1>
+        <img src={me} width="50%" height="50%"/>
       </div>
     )
   }
