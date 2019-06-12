b198e8dbdf7abd3d5acbabf23bfe259f4d8502e4
jbharwood <josephblakeharwood@gmail.com>
2019-06-12 14:08:50 -0400

    changed home route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <nav>
         <ul>
-          <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+          <li><NavLink exact activeClassName="current" to='/home'>Home</NavLink></li>
           <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
           <li><NavLink exact activeClassName="current" to='/contact'>Contact</NavLink></li>
         </ul>
