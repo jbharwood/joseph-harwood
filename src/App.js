7c725317b085cf116740e72217e0c9a869701a3d
jbharwood <josephblakeharwood@gmail.com>
2019-06-12 14:06:11 -0400

    added redirect to import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import NavBar from './components/NavBar';
-import { NavLink, Switch, Route } from 'react-router-dom';
+import { NavLink, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
