bc04049a39f79792185314dacdbf8d6aa9933a1c
jbharwood <josephblakeharwood@gmail.com>
2019-06-20 17:50:21 -0400

    added boxes and changed margins

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,16 @@ class About extends React.Component {
       <div className="body">
         <h1> About </h1>
         <div className="bodyContainer">
-          <p> Full-stack web developer with a passion for creating unique user
-          experiences using Ruby on Rails, JavaScript, and React and a background
-          in database administration. A gaming enthusiast, I bring strong skills
-          in cross-functional projects that meet customer demands and deadlines in
-          small environments as well as giants such as Blue Cross Blue Shield of
-          South Carolina serving millions of people.</p>
-          <img src={me} width="50%" height="50%"/>
+          <div className="description">
+            <p> Full-stack web developer with a passion for creating unique user
+            experiences using Ruby on Rails, JavaScript, and React and a background
+            in database administration. A gaming enthusiast, I bring strong skills
+            in cross-functional projects that meet customer demands and deadlines in
+            small environments as well as giants such as Blue Cross Blue Shield of
+            South Carolina serving millions of people.</p>
+          </div>
+          <br/>
+          <img src={me} width="50%" height="50%" class="picMe"/>
         </div>
       </div>
     )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,21 +9,25 @@ class Contact extends React.Component {
     return (
       <div class="body">
         <h1> Contact </h1>
-        <div className="bodyContainer" align="left">
-          <p>Feel free to send me an email or contact me on any of these sites:</p>
-          <img align="left" src={linkedin} alt="LinkedIn" title="LinkedIn" className="site_image"/>
-          <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
-          <br/>
-          <br/>
-          <img align="left" src={github} alt="Github" title="Github" className="site_image"/>
-          <a href="https://github.com/jbharwood" target="_blank"> Github </a>
-          <br/>
-          <br/>
-          <img align="left" src={medium} alt="Blog" title="Blog" className="site_image"/>
-          <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
-          <br/>
-          <br/>
-          <p> josephblakeharwood@gmail.com </p>
+        <div className="bodyContainer">
+          <div className="contactBody" align="left">
+            <div className="margin" align="left">
+              <p>Feel free to send me an email or contact me on any of these sites:</p>
+              <img align="left" src={linkedin} alt="LinkedIn" title="LinkedIn" className="site_image"/>
+              <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
+              <br/>
+              <br/>
+              <img align="left" src={github} alt="Github" title="Github" className="site_image"/>
+              <a href="https://github.com/jbharwood" target="_blank"> Github </a>
+              <br/>
+              <br/>
+              <img align="left" src={medium} alt="Blog" title="Blog" className="site_image"/>
+              <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
+              <br/>
+              <br/>
+              <p> josephblakeharwood@gmail.com </p>
+              </div>
+          </div>
         </div>
       </div>
     )
