001c9876b1f6b4af3e3407a279236168efed7c6d
jbharwood <josephblakeharwood@gmail.com>
2019-06-20 13:55:03 -0400

    changed formatting on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,25 +10,18 @@ class Contact extends React.Component {
       <div class="body">
         <h1> Contact </h1>
         <div className="bodyContainer" align="left">
-        <p>Feel free to send me an email or contact me on any of these sites:</p>
-          <div className="col1">
-            <div class="contactLinks">
-              <img align="left" src={linkedin} alt="LinkedIn" title="LinkedIn" className="site_image"/>
-              <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
-            </div>
-            <div class="contactLinks">
-              <img align="left" src={github} alt="Github" title="Github" className="site_image"/>
-              <a href="https://github.com/jbharwood" target="_blank"> Github </a>
-            </div>
-            <div class="contactLinks">
-              <img align="left" src={medium} alt="Blog" title="Blog" className="site_image"/>
-              <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
-            </div>
-          </div>
-          <div className="col2">
-            <br/>
-            <p> josephblakeharwood@gmail.com </p>
-          </div>
+          <p>Feel free to send me an email or contact me on any of these sites:</p>
+          <img align="left" src={linkedin} alt="LinkedIn" title="LinkedIn" className="site_image"/>
+          <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
+          <br/>
+          <img align="left" src={github} alt="Github" title="Github" className="site_image"/>
+          <a href="https://github.com/jbharwood" target="_blank"> Github </a>
+          <br/>
+          <img align="left" src={medium} alt="Blog" title="Blog" className="site_image"/>
+          <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
+          <br/>
+          <br/>
+          <p> josephblakeharwood@gmail.com </p>
         </div>
       </div>
     )
