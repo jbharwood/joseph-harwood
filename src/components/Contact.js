2bb213c3f5adc190aa57fb6e6672c5fb41e6b873
jbharwood <josephblakeharwood@gmail.com>
2019-06-20 14:20:03 -0400

    added 2 breaks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,8 +14,10 @@ class Contact extends React.Component {
           <img align="left" src={linkedin} alt="LinkedIn" title="LinkedIn" className="site_image"/>
           <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
           <br/>
+          <br/>
           <img align="left" src={github} alt="Github" title="Github" className="site_image"/>
           <a href="https://github.com/jbharwood" target="_blank"> Github </a>
+          <br/>
           <br/>
           <img align="left" src={medium} alt="Blog" title="Blog" className="site_image"/>
           <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
