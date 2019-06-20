df7ba6cf6131db75564f8fc894a670e5d066d3bf
jbharwood <josephblakeharwood@gmail.com>
2019-06-20 12:18:41 -0400

    changed contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import linkedin from '../media/linkedincolor.png'
+import github from '../media/github.png'
+import medium from '../media/medium.png'
 
 class Contact extends React.Component {
 
@@ -6,17 +9,23 @@ class Contact extends React.Component {
     return (
       <div class="body">
         <h1> Contact </h1>
-        <div className="bodyContainer">
-          <h3> Email</h3>
-          <p> josephblakeharwood@gmail.com </p>
-          <div class="contactLinks">
-            <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
+        <div className="contactContainer">
+          <div className="col1">
+            <div class="contactLinks">
+              <img align="left" src={linkedin} alt="LinkedIn" title="LinkedIn" className="site_image"/>
+              <a href="https://www.linkedin.com/in/josephblakeharwood/" target="_blank"> LinkedIn </a>
+            </div>
+            <div class="contactLinks">
+              <img align="left" src={github} alt="Github" title="Github" className="site_image"/>
+              <a href="https://github.com/jbharwood" target="_blank"> Github </a>
+            </div>
+            <div class="contactLinks">
+              <img align="left" src={medium} alt="Blog" title="Blog" className="site_image"/>
+              <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
+            </div>
           </div>
-          <div class="contactLinks">
-            <a href="https://github.com/jbharwood" target="_blank"> Github </a>
-          </div>
-          <div class="contactLinks">
-            <a href="https://medium.com/@josephharwood_62087" target="_blank"> Blog </a>
+          <div className="col2">
+            <p> josephblakeharwood@gmail.com </p>
           </div>
         </div>
       </div>
