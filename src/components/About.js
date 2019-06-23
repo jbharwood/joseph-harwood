73a6fc958f71e091a9663be37896ab92151369ae
jbharwood <josephblakeharwood@gmail.com>
2019-06-23 12:47:39 -0400

    changed about margins

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,14 @@ class About extends React.Component {
       <div className="body">
         <h1> About </h1>
         <div>
-          <div className="description">
-            <p> I am a full-stack web developer with a passion for creating unique user
-            experiences using Ruby on Rails, JavaScript, and React and a background
-            in database administration. A gaming enthusiast, bedroom guitarist, music lover,
-            film fanatic, chronic podcast listener, and Charleston native living in Brooklyn.
-            </p>
+          <div className="description" align="left">
+            <div className="margin" align="left">
+              <p> I am a full-stack web developer with a passion for creating unique user
+              experiences using Ruby on Rails, JavaScript, and React, and a background
+              in database administration. A gaming enthusiast, bedroom guitarist, music lover,
+              film fanatic, chronic podcast listener, and Charleston native living in Brooklyn.
+              </p>
+            </div>
           </div>
           <br/>
           <img src={me} width="50%" height="50%" class="picMe"/>
